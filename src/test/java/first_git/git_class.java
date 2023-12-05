9116b1675957610e138d6614b53105964af4acfb
vandana899 <vandanaraheja111@gmail.com>
2023-12-05 17:28:56 +0530

    gitcommands

---
+++ src/test/java/first_git/git_class.java
@@ -0,0 +1,14 @@
+package first_git;
+
+import org.testng.annotations.Test;
+
+public class git_class {
+@Test
+public void bb() {
+	System.out.println("1");
+	System.out.println("2");
+	System.out.println("3");
+	System.out.println("4");
+	System.out.println("5");
+}
+}
